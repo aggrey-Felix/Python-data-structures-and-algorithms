a160be2c3b71c511f5686f826e69b05b3a617bc5
aggrey-Felix <aggreyfelix@yahoo.com>
2022-06-07 22:42:37 -0700

    codes on python list

---
+++ DataStructures/Lists.py
@@ -0,0 +1,29 @@
+# Python List Operations
+
+# Creating a List with
+# the use of multiple values
+List = [345, "Grey", "546",54j, 0.46573]
+print("\nList containing multiple values: ")
+print(List)
+
+# Creating a Multi-Dimensional List
+# (By Nesting a list inside a List)
+List2 = [['maybe', 'hes'], ['whos']]
+print("\nMulti-Dimensional List: ")javascript:void(0);
+print(List2)
+
+# accessing a element from the
+# list using index number
+print("Accessing element from the list")
+print(List[0])
+print(List[2])
+
+# accessing a element using
+# negative indexing
+print("Accessing element using negative indexing")
+	
+# print the last element of list
+print(List[-1])
+	
+# print the third last element of list
+print(List[-3])
